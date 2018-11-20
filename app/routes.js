1299299195b1034cdabf9e6c230c1d135791e1c5
Kuba Bartwicki <kuba.bartwicki@digital.cabinet-office.gov.uk>
2018-11-20 11:19:59 +0000

    New service name for -transfer files

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,10 @@ router.get('/*-submit', function(req, res, next){
   res.locals['serviceName'] = 'Submit emissions data for ETS'
   next()
 })
+router.get('/*-transfer', function(req, res, next){
+  res.locals['serviceName'] = 'Check and transfer allowance for ETS'
+  next()
+})
 
 
 module.exports = router
